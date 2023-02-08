43d1920cabdbb1f7b158a9fdc4b196ef122069fc
ArtavazdGevorgyan <ag15012003@gmail.com>
2023-02-08 19:01:36 +0400

    Add files via upload

---
+++ BV.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class BitVector:
+    def __init__(self, length: int):
+        """
+        Args:
+            length (int): length of BitVector
+        """
+        self.__arr = np.zeros((length+63)//64, dtype=np.uint64)
+
+    def __repr__(self):
+        return f"{self.__arr[::-1]}"
+
+    def set(self, index: int):
+        """Changes index of a BitVector to 1
+
+        Args:
+            index (int): the index of BitVector to be modified
+        """
+        self.__arr[index // 64] |= np.uint64(1 << (index % 64))
+
+    def reset(self, index: int):
+        """Changes index of a BitVector to 0
+
+        Args:
+            index (int): the index of BitVector to be modified
+        """
+        self.__arr[index // 64] ^= np.uint64(1 << (index % 64))
